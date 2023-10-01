b1859e75aace0c6e4f8159402022f7afbc9295ed
Aria Fatah <ariafatah0711@gmail.com>
2023-10-01 18:00:42 +0700

    update

--- script/navbar.js
+++ script/navbar.js
@@ -1,15 +1,14 @@
 // let menuBtn = document.getElementById(`menu-btn`);
-let menuBtn = document.querySelector(`#menu-btn`);
+// let menuBtn = document.querySelector(`#menu-btn`);
 let navbar = document.querySelector(`.navbar`);
 let navbarA = document.querySelectorAll(`.navbar a`);
 
 let aAll = document.querySelectorAll(`a`);
 let aIndex6 = [];
-for (let i = 5; i < aAll.length; i++) {
+for (let i = 7; i < aAll.length; i++) {
   aIndex6.push(aAll[i]);
 }
 
-console.log(menuBtn);
 // ===== NAVBAR =====
 const Button = {
   remove: function () {
@@ -48,13 +47,6 @@ for (const a of aIndex6) {
   });
 }
 
-function showNavbar() {
-  document.getElementById(`menu-btn`).onclick = () => {
-    document.querySelector(`.navbar`).classList.toggle(`active`);
-  };
-  // navbar.classList.toggle(`active`);
-}
-
 // ===== mobile =====
 function cnv() {
   let navbar = document.querySelector(`.navbar`);
@@ -64,7 +56,7 @@ function cnv() {
 // ===== desktop =====
 
 // menuBtn => show navbar saat icon di click
-menuBtn.onclick = () => navbar.classList.toggle(`active`);
+// menuBtn.onclick = () => navbar.classList.toggle(`active`);
 
 // scroll => saat di scroll akan meremove class active
 window.onscroll = () => navbar.classList.remove(`active`);
